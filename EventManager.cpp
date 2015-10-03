822f73fb20dd890c5f3a8db728e9ae20b77d61c1
djeck <djeck.det@gmail.com>
2015-10-03 21:37:58 +0200

    correction

--- EventManager.cpp
+++ EventManager.cpp
@@ -10,9 +10,9 @@ void EventManager::build()
 
 }
 
-void EventManager::setCloseCallback(void (*callback)() )
+void EventManager::setCloseCallback(sf::RenderWindow* window )
 {
-	m_callback=callback;
+	m_ptrWindow=window;
 }
 
 void EventManager::run(sf::Event event)
@@ -24,35 +24,32 @@ void EventManager::run(sf::Event event)
      }
      else if (event.type == sf::Event::Closed)
      {
-		m_callback();
+		close();
      }
      else if(event.key.code == sf::Keyboard::Escape)
 	{
-		m_callback();
+		close();
 	}
 	else if(event.type == sf::Event::KeyPressed)
 	{
 	      switch(event.key.code)
 	      {
 		case sf::Keyboard::A :
-		  player.setRotationMV(sf::Vector3f(0,25,50));
 		  break;
 		case sf::Keyboard::S :
-		  camera.setVitesseFlying(sf::Vector3f(0,0,-3));
 		  break;
 		case sf::Keyboard::Z :
-		  camera.setVitesseFlying(sf::Vector3f(0,0,3));
 		  break;
 		case sf::Keyboard::Q :
-		  camera.setVitesseFlying(sf::Vector3f(3,0,0));
 		  break;
 		case sf::Keyboard::D :
-		  camera.setVitesseFlying(sf::Vector3f(-3,0,0));
 		  break;
 		case sf::Keyboard::E:
-		  player.setVitesse(sf::Vector3f(-3,0,0));
-		  player2.setVitesse(sf::Vector3f(3,0,0));
 		break;
 	      }
 	}
+}
+void EventManager::close()
+{
+	m_ptrWindow->close();
 }
--- EventManager.hpp
+++ EventManager.hpp
@@ -2,17 +2,18 @@
 #define EVENT_MANAGER
 
 #include <SFML/Graphics.hpp>
-#include "personne.hpp"
+#include "Object.hpp"
 
 class EventManager
 {
 public:
 	EventManager();
-	void setCloseCallback(void (*callback)());
+	void setCloseCallback(sf::RenderWindow* window);
 	void run(sf::Event event);
-	void (*m_callback)();
 private:
 	void build();
+	void close();
+	sf::RenderWindow* m_ptrWindow;
 	
 };
 #endif
